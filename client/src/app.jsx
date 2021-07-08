a1c4086582120a1ce2523a4a6ed05578c46a425f
dhanushthakkalapally <69356554+dhanusht97@users.noreply.github.com>
2021-07-08 21:53:35 +0100

    intial commit for client

---
+++ client/src/app.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className =       "  App-header">
+        Hello from Fitness App
+      </header>
+    </div>
+  );
+}
+
+export default App;
