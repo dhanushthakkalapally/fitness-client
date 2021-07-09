46fb3dd0e8992867976156b5d766af6605abf035
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-09 19:45:54 +0100

    added tables and toRelease files and sql queries

---
+++ _developer/sql/toRelease.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Role(
+    id tinyint NOT NULL,
+    name varchar(100),
+    CONSTRAINT PK_Role PRIMARY KEY (id ASC),
+    CONSTRAINT UQ_Role_id UNIQUE (name)
+);
+CREATE TABLE User(
+    dateCreated datetime DEFAULT (current_timestamp()),
+    dateModified datetime DEFAULT (current_timestamp()),
+    id int AUTO_INCREMENT NOT NULL,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    dobYear int NOT NULL,
+    active binary NOT NULL DEFAULT (1),
+    dobMonth int NOT NULL,
+    roleId tinyint NOT NULL,
+    CONSTRAINT PK_User PRIMARY KEY (id ASC),
+    CONSTRAINT FK_User_Role FOREIGN KEY (roleId) REFERENCES Role(id)
+);
+CREATE TABLE UserLogin(
+    dateCreated datetime DEFAULT (current_timestamp()) NOT NULL,
+    dateModified datetime DEFAULT (current_timestamp()) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(512) NOT NULL,
+    userId int NOT NULL,
+    CONSTRAINT PK_UserLogin PRIMARY KEY(email ASC),
+    CONSTRAINT FK_User_userId FOREIGN KEY (userId) REFERENCES User(id)
+);
+
+delete from Role;
+
+insert into Role(id, name)
+values (1, 'User'),
+       (2, 'SuperAdmin');
