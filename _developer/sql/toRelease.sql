bd7e8517005304eae3a5c8a72cd68cb45e518c65
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-09 19:51:20 +0100

    added drop statements

--- _developer/sql/toRelease.sql
+++ _developer/sql/toRelease.sql
@@ -1,9 +1,14 @@
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS UserLogin;
+
 CREATE TABLE Role(
     id tinyint NOT NULL,
     name varchar(100),
     CONSTRAINT PK_Role PRIMARY KEY (id ASC),
     CONSTRAINT UQ_Role_id UNIQUE (name)
 );
+
 CREATE TABLE User(
     dateCreated datetime DEFAULT (current_timestamp()),
     dateModified datetime DEFAULT (current_timestamp()),
@@ -17,6 +22,7 @@ CREATE TABLE User(
     CONSTRAINT PK_User PRIMARY KEY (id ASC),
     CONSTRAINT FK_User_Role FOREIGN KEY (roleId) REFERENCES Role(id)
 );
+
 CREATE TABLE UserLogin(
     dateCreated datetime DEFAULT (current_timestamp()) NOT NULL,
     dateModified datetime DEFAULT (current_timestamp()) NOT NULL,
