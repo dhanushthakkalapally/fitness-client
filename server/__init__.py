4c964b5609bd77682610d48f1e942b8dab16da85
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-09 20:37:36 +0100

    added routes and util and decorators module

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
 from logging import basicConfig, INFO
 basicConfig(level=INFO)
-from .app import app
+from .app import app, Api
 from .db import db
---
+++ server/src/routes/__init__.py
@@ -0,0 +1,1 @@
+"""This module contains routes for the api"""
