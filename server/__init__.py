6d5e19e74a8b57208b9fde0fb8a01674600680a1
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-09 19:45:21 +0100

    added init and config file

--- server/__init__.py
+++ server/__init__.py
@@ -2,4 +2,3 @@ from logging import basicConfig, INFO
 basicConfig(level=INFO)
 from .app import app
 from .db import db
-
--- server/config.py
+++ server/config.py
@@ -1,8 +1,11 @@
 import os
+
+
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_SIZE = 25
     SQLALCHEMY_DATABASE_URI = os.environ['CONNECTION_STRING']
+
 
 class DevelopmentConfig(Config):
     pass
