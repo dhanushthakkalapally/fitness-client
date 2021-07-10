48f84b9c460f388c00e8b68a70b526b635409444
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-10 13:42:15 +0100

    added client initial project structure

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,5 @@
 from logging import basicConfig, INFO
-basicConfig(level=INFO)
+basicConfig(level=INFO, format='%(asctime)s:%(name)s:%(message)s')
 from .app import app, api
 from .db import engine
 from .src import routes
--- server/src/util/security_util.py
+++ server/src/util/security_util.py
@@ -1,14 +1,18 @@
 import jwt
 from ... import app
 import datetime
+
 secret_key = app.config['SECRET_KEY']
 jwt_expiry = app.config['JWT_EXPIRY']
 
-print(secret_key)
+
 def encode(user_id):
-    return jwt.encode({'userId': user_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=jwt_expiry),
+    return jwt.encode({'userId': user_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=10),
                        'iat': datetime.datetime.utcnow()}, key=secret_key)
 
 
 def decode(payload):
-    return jwt.decode(payload, secret_key)
+    try:
+        return jwt.decode(payload, secret_key, algorithms="HS256")
+    except jwt.ExpiredSignatureError as e:
+        return False
