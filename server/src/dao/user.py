0db8953245ddb150f7d16ac3fd6095fddf5fafcd
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-10 03:28:52 +0100

    updated to only one proc call to validate and get the user

--- server/src/dao/user.py
+++ server/src/dao/user.py
@@ -30,16 +30,12 @@ class User:
             row = next(cursor.stored_results()).fetchone()
             cursor.close()
             if row:
-                cursor = conn.cursor()
                 user_id = row[0]
-                proc_name = 'User_GetByUserIdForAuth'
-                cursor.callproc(proc_name, [user_id])
-                row = next(cursor.stored_results()).fetchone()
-                first_name = row[0]
-                last_name = row[1]
-                dob_year = row[2]
-                dob_month = row[2]
-                role_id = row[4]
+                first_name = row[1]
+                last_name = row[2]
+                dob_year = row[3]
+                dob_month = row[4]
+                role_id = row[5]
                 cursor.close()
                 return cls(user_id, first_name, last_name, dob_year, dob_month, role_id)
             else:
@@ -47,6 +43,16 @@ class User:
         except user.InvalidCredentials as e:
             raise user.InvalidCredentials(e)
         except Exception as e:
-                raise e
+            raise e
         finally:
             conn.close()
+
+    # @classmethod
+    # def register_user(cls, first_name, last_name, email, password, dob_year, dob_month, role_id=1):
+    #     """This method registers user and returns user object"""
+    #     conn = engine.raw_connection()
+    #     try:
+    #         cursor = conn.cursor()
+    #         # Check if the user already exists
+    #         proc_name =
+    #         cursor.callproc('')
