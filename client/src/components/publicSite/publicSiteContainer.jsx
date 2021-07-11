a438f3b35f5523fdf666d71c5b0faea0a9355ce9
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-11 01:33:39 +0100

    made public siteContainer to load the components lazily

--- client/src/components/publicSite/publicSiteContainer.jsx
+++ client/src/components/publicSite/publicSiteContainer.jsx
@@ -1,20 +1,49 @@
-import React, {Component} from "react";
+import React, {Component, lazy, Suspense} from "react";
 import PublicSiteHeader from "./publicSiteHeader";
+import {Route} from "react-router";
+
 
 class PublicSiteContainer extends Component {
     constructor(props, context) {
         super(props, context);
+        this.componentsCache = {};
     }
 
     render() {
+        const {path} = this.props;
+        const Component = this.getComponentInstance();
         return (
             <>
                 <section>
                     <PublicSiteHeader/>
                 </section>
+                <Route path={path} exact render={
+                    props => {
+                        return (
+                            <>
+                                <Suspense fallback={<></>}>
+                                    <Component {...props}>
+                                    </Component>
+                                </Suspense>
+                            </>
+                        )
+                    }
+                }>
+                </Route>
+
             </>
         )
 
+    }
+
+    getComponentInstance() {
+        const {routeName} = this.props;
+        if (!this.componentsCache[routeName]) {
+            //This will import required component lazily only when required
+            this.componentsCache[routeName] = lazy(() => import('./pages/' + routeName));
+        }
+
+        return this.componentsCache[routeName];
     }
 }
 
