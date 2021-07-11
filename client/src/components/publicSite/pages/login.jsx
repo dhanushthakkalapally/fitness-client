ec5e0f0d11a9fef710698412667317504a6b166e
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-11 13:46:14 +0100

    login page sets the auth on successful authentication

--- client/src/components/publicSite/pages/login.jsx
+++ client/src/components/publicSite/pages/login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BaseComponent from "../../baseComponent";
 import LoginForm from "../element/loginForm";
 import {connect} from 'react-redux';
+import {setAuth} from "../../../store/actions/authAction";
 
 const mapStateToProps = state => {
     return {
@@ -9,10 +10,17 @@ const mapStateToProps = state => {
     }
 };
 
-
+const mapDispatchToProps = dispatch => {
+    return {
+        setAuth: details => {
+            dispatch(setAuth(details))
+        }
+    }
+};
 
 class Login extends BaseComponent {
     signInHandler = (email, password) => {
+        const {setAuth} = this.props;
         const postData = {
             email,
             password
@@ -20,18 +28,18 @@ class Login extends BaseComponent {
         this.apiPost('signIn', 'user/login', postData, res => {
             //Now deal with sign in so when the user logged in
             // set the item in local storage and take it with every next request
-
-            console.log(res.data);
+            const {data} = res;
+            setAuth(data)
         }, err => {
+            console.error(err);
         })
     };
 
     render() {
-        console.log(this.props);
         return (<>
             <LoginForm signInHandler={this.signInHandler}/>
         </>)
     }
 }
 
-export default connect(mapStateToProps, null)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
