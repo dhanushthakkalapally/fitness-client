6eb09c39fd7b2326874a49ec5bd5bd627378c54a
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-11 01:52:12 +0100

    added method to build the props for routing

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,32 @@
+import React from "react";
+import BaseComponent from "./components/baseComponent";
 import {Switch} from "react-router";
-import {Route} from "react-router";
-import {Link} from "react-router-dom";
 import PublicSiteContainer from "./components/publicSite/publicSiteContainer";
+import getPathUrl from "./utils/routesUtil";
 
-function App() {
-    return (
-        <>
-            <PublicSiteContainer/>
-        </>
-    );
+class App extends BaseComponent {
+
+    static buildRouteParams(componentName, exact = true) {
+        const path = getPathUrl(componentName);
+        return {
+            componentName,
+            path,
+            exact
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Switch>
+                    {/*Switch make the components inside it singleton that means publicSiteContainer will only be initialized once and upon next call only render is called
+                 and componentDidUpdate **need to get more understanding*** */}
+                    <PublicSiteContainer {...App.buildRouteParams('landingPage')}/>
+                </Switch>
+            </>
+        );
+    }
 }
+
 
 export default App;
