344f2934d0ee482b49211abc7084d04ee6738762
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-11 01:07:21 +0100

    changed to functional component

--- client/src/components/publicSite/publicSiteHeader.jsx
+++ client/src/components/publicSite/publicSiteHeader.jsx
@@ -1,32 +1,30 @@
-import React, {Component} from "react";
-import {Navbar, Nav} from "react-bootstrap";
+import React from "react";
+import {Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import './styles/publicHeader.css';
 
-class PublicSiteHeader extends Component {
-    render() {
-        return (
-            <>
-                <Navbar expand="lg" className="navBar">
-                    <div className="container">
-                        <Navbar.Brand className="navBrand">
-                            Fitness
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                        <Navbar.Collapse className="justify-content-between" id="responsive-navbar-nav">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item navElement"><Link to="/">Home</Link></li>
-                            </ul>
-                            <ul className="navbar-nav">
-                                <li className="nav-item navElement me-2"><Link to="/about">About</Link></li>
-                                <li className="nav-item navElement me-2"><Link to="/login">Login</Link></li>
-                            </ul>
-                        </Navbar.Collapse>
-                    </div>
-                </Navbar>
-            </>)
-    }
-}
+function PublicSiteHeader(props) {
+    return (
+        <>
+            <Navbar expand="lg" className="navBar">
+                <div className="container">
+                    <Navbar.Brand className="navBrand">
+                        Fitness
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse className="justify-content-between" id="responsive-navbar-nav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item navElement"><Link to="/">Home</Link></li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item navElement me-2"><Link to="/about">About</Link></li>
+                            <li className="nav-item navElement me-2"><Link to="/login">Login</Link></li>
+                        </ul>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
+        </>)
+};
 
 
 export default PublicSiteHeader;
