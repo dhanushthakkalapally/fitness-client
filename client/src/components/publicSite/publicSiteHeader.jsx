4397b8f6c52a48f9812edfdba9d68135ae562de7
dhanushthakkalapally <65690532+dhanushthakkalapally@users.noreply.github.com>
2021-07-11 13:55:16 +0100

    publicHeader not need for authentication

--- client/src/components/publicSite/publicSiteHeader.jsx
+++ client/src/components/publicSite/publicSiteHeader.jsx
@@ -2,45 +2,30 @@ import React, {Component} from "react";
 import {Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import './styles/publicHeader.css';
-import {connect} from "react-redux";
-import {Redirect} from "react-router";
-import {Routes} from "../../utils/routesUtil";
 
-const mapStateToProps= state=> {
-  return {
-      auth: state.auth
-  }
+
+function PublicSiteHeader(props) {
+    return (
+        <>
+            <Navbar expand="lg" className="navBar">
+                <div className="container">
+                    <Navbar.Brand className="navBrand">
+                        Fitness
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse className="justify-content-between" id="responsive-navbar-nav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item navElement"><Link to="/">Home</Link></li>
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item navElement me-2"><Link to="/about">About</Link></li>
+                            <li className="nav-item navElement me-2"><Link to="/login">Sign in</Link></li>
+                        </ul>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
+        </>)
 };
 
 
-class PublicSiteHeader extends Component {
-
-
-    render() {
-        const {isAuthenticated} = this.props.auth;
-        return (
-            <>
-                <Navbar expand="lg" className="navBar">
-                    <div className="container">
-                        <Navbar.Brand className="navBrand">
-                            Fitness
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                        <Navbar.Collapse className="justify-content-between" id="responsive-navbar-nav">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item navElement"><Link to={Routes.landingPage.url}>Home</Link></li>
-                            </ul>
-                            <ul className="navbar-nav">
-                                <li className="nav-item navElement me-2"><Link to="/about">About</Link></li>
-                                {!isAuthenticated && <li className="nav-item navElement me-2"><Link to={Routes.login.url}>Sign in</Link></li>}
-                                {isAuthenticated && <Redirect to={Routes.dashboard.url}/>}
-                            </ul>
-                        </Navbar.Collapse>
-                    </div>
-                </Navbar>
-            </>)
-    }
-};
-
-
-export default connect(mapStateToProps, null)(PublicSiteHeader);
+export default PublicSiteHeader;
