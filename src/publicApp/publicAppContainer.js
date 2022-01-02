5ad948f0aee1d77d306cd9d4e440308dbb65fccc
Dhanush97 <dhanush@evinox.co.uk>
2022-01-02 23:55:45 +0000

    added app headers for public and private application

--- src/publicApp/publicAppContainer.js
+++ src/publicApp/publicAppContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useSelector} from "react-redux";
 import {Redirect, Route, Switch} from "react-router";
 import publicAppRoutes from "../publicApp/publicAppRoutes";
+import PublicAppHeader from "./publicAppHeader";
 
 const PublicAppContainer = () => {
     const {auth} = useSelector(state => {
@@ -10,18 +11,20 @@ const PublicAppContainer = () => {
         }
     })
     const {isAuthenticated} = auth;
-    console.log(isAuthenticated)
     return (
-        <Switch>
-            {publicAppRoutes.map((item, idx) => <Route path={item.path} render={props => {
-                const {canAllow, component: Component} = item;
-                if ((isAuthenticated && canAllow) || !isAuthenticated) {
-                    return <Component props={props}/>
-                }
-                return <Redirect to="/dashboard"/>
-            }} idx={idx}/>)}
+        <>
+            {!isAuthenticated && <PublicAppHeader/>}
+            <Switch>
+                {publicAppRoutes.map((item, idx) => <Route path={item.path} render={props => {
+                    const {canAllow, component: Component} = item;
+                    if ((isAuthenticated && canAllow) || !isAuthenticated) {
+                        return <Component props={props}/>
+                    }
+                    return <Redirect to="/dashboard"/>
+                }} idx={idx}/>)}
 
-        </Switch>
+            </Switch>
+        </>
     )
 }
 
