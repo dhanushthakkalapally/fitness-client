a1d9c786a873d877ebc1974c15c80b1cdb36ca80
Dhanush97 <dhanush@evinox.co.uk>
2022-01-03 00:57:41 +0000

    added login form and updated some styles

--- src/publicApp/pages/login.jsx
+++ src/publicApp/pages/login.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import {Form, Formik} from "formik";
+import InputElement from "../../sharedInteface/inputElement";
 
 
 const Login = props => {
     return (
-        <h2> Hello from the react application</h2>
+        <>
+            <div className="basicCard utPosCenter h-50 w-25">
+                <h3 className="text-center">WELCOME</h3>
+                <Formik initialValues={{email: "", password: ""}} onSubmit={() => console.log("gelkj")}>
+                    {() => (<Form>
+                        <div className="p-2">
+                            <InputElement label="Email:"
+                                          required
+                                          type="email"
+                                          id="email"
+                                          name="email"
+                            />
+                        </div>
+                        <div className="p-2">
+                            <InputElement label="Password:"
+                                          required
+                                          type="password"
+                                          id="password"
+                                          name="password"
+                            />
+                        </div>
+                        <div className="p-2">
+                            <button type="submit"
+                                    className="btn btn-block btn-primary btn-md"
+                            >
+                                Login
+                            </button>
+                        </div>
+                    </Form>)
+
+                    }
+                </Formik>
+            </div>
+        </>
     )
 }
 
