965de5af8031c1fc0544b5d7a0612c37dbe8a662
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-07-30 22:24:26 +0100

    added cloudformation command

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 // Command to create new stack
 aws cloudformation create-stack 
     --stack-name fitness-client-stack-1 
-    --template-body file://template.yml
+    --template-body file://template.ym
