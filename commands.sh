ed8e0abed04fe4a4ae77bda4d7bef6c4ef967138
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-09-01 00:40:31 +0100

    Testing for [CodeBuild] to work

--- commands.sh
+++ commands.sh
@@ -2,9 +2,9 @@
 aws cloudformation create-stack 
     --stack-name fitness-client-stack-2
     --template-body file://template.yml
-    --capabilities CAPABILITY_NAMED_IAM
+    --capabilities CAPABILITY_NAMED_IAMI
 
 aws cloudformation update-stack
   --stack-name fitness-client-stack-2
   --template-body file://template.yml
-  --capabilities CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_NAMED_IAMI
