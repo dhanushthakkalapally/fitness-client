22e67589aa8a670b465169d677cb4206aee6e4a3
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-09-02 01:51:19 +0100

    created security group

--- commands.sh
+++ commands.sh
@@ -4,4 +4,4 @@ aws cloudformation create-stack
     --template-body file://template.yml
     --capabilities CAPABILITY_NAMED_IAMI
 
-aws cloudformation update-stack --stack-name fitness-client-stack-2 --template-body file://template.yml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation update-stack --stack-name fitness-client-stack-2 --template-body file://template.yml --parameters ParameterKey=MyIP,ParameterValue=82.9.11.7 --capabilities CAPABILITY_NAMED_IAM
