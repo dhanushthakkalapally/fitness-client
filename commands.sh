3e8e01609ab7674a7f5d3eaaceae9d08f9b1a9a7
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2022-09-03 10:30:07 +0100

    updated create-stack command [CodeBuild]

--- commands.sh
+++ commands.sh
@@ -1,7 +1,4 @@
 # Command to create new stack
-aws cloudformation create-stack 
-    --stack-name fitness-client-stack-2
-    --template-body file://template.yml
-    --capabilities CAPABILITY_NAMED_IAMI
+aws cloudformation create-stack --stack-name fitness-client-stack-2 --template-body file://template.yml --parameters ParameterKey=MyIP,ParameterValue=82.9.11.7/32 --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation update-stack --stack-name fitness-client-stack-2 --template-body file://template.yml --parameters ParameterKey=MyIP,ParameterValue=82.9.11.7 --capabilities CAPABILITY_NAMED_IAM
